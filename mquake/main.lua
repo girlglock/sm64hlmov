1678a118e6965640540e50a394ac96d4fcdb72b7
Bryan Haley <bryanhaley.professional@gmail.com>
2024-11-27 09:39:39 -0800

    Replace compiled mquake with mquake source

--- mquake/main.lua
+++ mquake/main.lua
@@ -1,6 +1,4 @@
--- name: mQuake
+-- name: mQuake [Development]
 -- incompatible: moveset 
--- description: Quake style physics.\nCreated by 0x2480 with help from Agent X\n\n[Beta 2 Early Release]
+-- description: 
 -- deluxe: true
-
--- mQuake uncompiled is no longer available publicly. If you wish to get the code, message me.
